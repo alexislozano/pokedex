64e48cd6334475d1360b1262918e599ff5f24e7e
Alexis Lozano <alexis.pascal.lozano@gmail.com>
2021-10-17 21:33:51 +0200

    article-7

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,47 @@ extern crate rouille;
 extern crate clap;
 extern crate serde;
 
-use clap::{App, Arg};
-use repositories::pokemon::InMemoryRepository;
+use clap::{App, Arg, Values};
+use repositories::pokemon::{AirtableRepository, InMemoryRepository, Repository, SqliteRepository};
 use std::sync::Arc;
 
 fn main() {
-    let repo = Arc::new(InMemoryRepository::new());
-
     let matches = App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
         .arg(Arg::with_name("cli").long("cli").help("Runs in CLI mode"))
+        .arg(Arg::with_name("sqlite").long("sqlite").value_name("PATH"))
+        .arg(
+            Arg::with_name("airtable")
+                .long("airtable")
+                .value_names(&["API_KEY", "WORKSPACE_ID"]),
+        )
         .get_matches();
+
+    let repo = build_repo(matches.value_of("sqlite"), matches.values_of("airtable"));
 
     match matches.occurrences_of("cli") {
         0 => api::serve("localhost:8000", repo),
         _ => cli::run(repo),
     }
+}
+
+fn build_repo(sqlite_value: Option<&str>, airtable_values: Option<Values>) -> Arc<dyn Repository> {
+    if let Some(values) = airtable_values {
+        if let [api_key, workspace_id] = values.collect::<Vec<&str>>()[..] {
+            match AirtableRepository::try_new(api_key, workspace_id) {
+                Ok(repo) => return Arc::new(repo),
+                _ => panic!("Error while creating airtable repo"),
+            }
+        }
+    }
+
+    if let Some(path) = sqlite_value {
+        match SqliteRepository::try_new(path) {
+            Ok(repo) => return Arc::new(repo),
+            _ => panic!("Error while creating sqlite repo"),
+        }
+    }
+
+    Arc::new(InMemoryRepository::new())
 }
--- src/repositories/pokemon.rs
+++ src/repositories/pokemon.rs
@@ -1,5 +1,8 @@
 use crate::domain::entities::{Pokemon, PokemonName, PokemonNumber, PokemonTypes};
-use std::sync::Mutex;
+use rusqlite::{params, params_from_iter, Connection, Error::SqliteFailure, OpenFlags};
+use serde::Deserialize;
+use std::convert::TryFrom;
+use std::sync::{Mutex, MutexGuard};
 
 pub enum InsertError {
     Conflict,
@@ -131,5 +134,374 @@ impl Repository for InMemoryRepository {
 
         lock.remove(index);
         Ok(())
+    }
+}
+
+pub struct AirtableRepository {
+    url: String,
+    auth_header: String,
+}
+
+impl AirtableRepository {
+    pub fn try_new(api_key: &str, workspace_id: &str) -> Result<Self, ()> {
+        let url = format!("https://api.airtable.com/v0/{}/pokemons", workspace_id);
+        let auth_header = format!("Bearer {}", api_key);
+
+        if let Err(_) = ureq::get(&url).set("Authorization", &auth_header).call() {
+            return Err(());
+        }
+
+        Ok(Self { url, auth_header })
+    }
+
+    fn fetch_pokemon_rows(&self, number: Option<u16>) -> Result<AirtableJson, ()> {
+        let url = match number {
+            Some(number) => format!("{}?filterByFormula=number%3D{}", self.url, number),
+            None => format!("{}?sort%5B0%5D%5Bfield%5D=number", self.url),
+        };
+
+        let res = match ureq::get(&url)
+            .set("Authorization", &self.auth_header)
+            .call()
+        {
+            Ok(res) => res,
+            _ => return Err(()),
+        };
+
+        match res.into_json::<AirtableJson>() {
+            Ok(json) => Ok(json),
+            _ => Err(()),
+        }
+    }
+}
+
+impl Repository for AirtableRepository {
+    fn insert(
+        &self,
+        number: PokemonNumber,
+        name: PokemonName,
+        types: PokemonTypes,
+    ) -> Result<Pokemon, InsertError> {
+        let json = match self.fetch_pokemon_rows(Some(u16::from(number.clone()))) {
+            Ok(json) => json,
+            _ => return Err(InsertError::Unknown),
+        };
+
+        if !json.records.is_empty() {
+            return Err(InsertError::Conflict);
+        }
+
+        let body = ureq::json!({
+            "records": [{
+                "fields": {
+                    "number": u16::from(number.clone()),
+                    "name": String::from(name.clone()),
+                    "types": Vec::<String>::from(types.clone()),
+                },
+            }],
+        });
+
+        if let Err(_) = ureq::post(&self.url)
+            .set("Authorization", &self.auth_header)
+            .send_json(body)
+        {
+            return Err(InsertError::Unknown);
+        }
+
+        Ok(Pokemon::new(number, name, types))
+    }
+
+    fn fetch_all(&self) -> Result<Vec<Pokemon>, FetchAllError> {
+        let json = match self.fetch_pokemon_rows(None) {
+            Ok(json) => json,
+            _ => return Err(FetchAllError::Unknown),
+        };
+
+        let mut pokemons = vec![];
+
+        for record in json.records.into_iter() {
+            match (
+                PokemonNumber::try_from(record.fields.number),
+                PokemonName::try_from(record.fields.name),
+                PokemonTypes::try_from(record.fields.types),
+            ) {
+                (Ok(number), Ok(name), Ok(types)) => {
+                    pokemons.push(Pokemon::new(number, name, types))
+                }
+                _ => return Err(FetchAllError::Unknown),
+            }
+        }
+
+        Ok(pokemons)
+    }
+
+    fn fetch_one(&self, number: PokemonNumber) -> Result<Pokemon, FetchOneError> {
+        let mut json = match self.fetch_pokemon_rows(Some(u16::from(number.clone()))) {
+            Ok(json) => json,
+            _ => return Err(FetchOneError::Unknown),
+        };
+
+        if json.records.is_empty() {
+            return Err(FetchOneError::NotFound);
+        }
+
+        let record = json.records.remove(0);
+
+        match (
+            PokemonNumber::try_from(record.fields.number),
+            PokemonName::try_from(record.fields.name),
+            PokemonTypes::try_from(record.fields.types),
+        ) {
+            (Ok(number), Ok(name), Ok(types)) => Ok(Pokemon::new(number, name, types)),
+            _ => Err(FetchOneError::Unknown),
+        }
+    }
+
+    fn delete(&self, number: PokemonNumber) -> Result<(), DeleteError> {
+        let mut json = match self.fetch_pokemon_rows(Some(u16::from(number.clone()))) {
+            Ok(json) => json,
+            _ => return Err(DeleteError::Unknown),
+        };
+
+        if json.records.is_empty() {
+            return Err(DeleteError::NotFound);
+        }
+
+        let record = json.records.remove(0);
+
+        match ureq::delete(&format!("{}/{}", self.url, record.id))
+            .set("Authorization", &self.auth_header)
+            .call()
+        {
+            Ok(_) => Ok(()),
+            _ => Err(DeleteError::Unknown),
+        }
+    }
+}
+
+#[derive(Deserialize)]
+struct AirtableJson {
+    records: Vec<AirtableRecord>,
+}
+
+#[derive(Deserialize)]
+struct AirtableRecord {
+    id: String,
+    fields: AirtableFields,
+}
+
+#[derive(Deserialize)]
+struct AirtableFields {
+    number: u16,
+    name: String,
+    types: Vec<String>,
+}
+
+pub struct SqliteRepository {
+    connection: Mutex<Connection>,
+}
+
+impl SqliteRepository {
+    pub fn try_new(path: &str) -> Result<Self, ()> {
+        let connection = match Connection::open_with_flags(path, OpenFlags::SQLITE_OPEN_READ_WRITE)
+        {
+            Ok(connection) => connection,
+            _ => return Err(()),
+        };
+
+        match connection.execute("pragma foreign_keys = 1", []) {
+            Ok(_) => Ok(Self {
+                connection: Mutex::new(connection),
+            }),
+            _ => Err(()),
+        }
+    }
+
+    fn fetch_pokemon_rows(
+        lock: &MutexGuard<'_, Connection>,
+        number: Option<u16>,
+    ) -> Result<Vec<(u16, String)>, ()> {
+        let (query, params) = match number {
+            Some(number) => (
+                "select number, name from pokemons where number = ?",
+                vec![number],
+            ),
+            _ => ("select number, name from pokemons", vec![]),
+        };
+
+        let mut stmt = match lock.prepare(query) {
+            Ok(stmt) => stmt,
+            _ => return Err(()),
+        };
+
+        let mut rows = match stmt.query(params_from_iter(params)) {
+            Ok(rows) => rows,
+            _ => return Err(()),
+        };
+
+        let mut pokemon_rows = vec![];
+
+        while let Ok(Some(row)) = rows.next() {
+            match (row.get::<usize, u16>(0), row.get::<usize, String>(1)) {
+                (Ok(number), Ok(name)) => pokemon_rows.push((number, name)),
+                _ => return Err(()),
+            };
+        }
+
+        Ok(pokemon_rows)
+    }
+
+    fn fetch_type_rows(lock: &MutexGuard<'_, Connection>, number: u16) -> Result<Vec<String>, ()> {
+        let mut stmt = match lock.prepare("select name from types where pokemon_number = ?") {
+            Ok(stmt) => stmt,
+            _ => return Err(()),
+        };
+
+        let mut rows = match stmt.query([number]) {
+            Ok(rows) => rows,
+            _ => return Err(()),
+        };
+
+        let mut type_rows = vec![];
+
+        while let Ok(Some(row)) = rows.next() {
+            match row.get::<usize, String>(0) {
+                Ok(name) => type_rows.push(name),
+                _ => return Err(()),
+            };
+        }
+
+        Ok(type_rows)
+    }
+}
+
+impl Repository for SqliteRepository {
+    fn insert(
+        &self,
+        number: PokemonNumber,
+        name: PokemonName,
+        types: PokemonTypes,
+    ) -> Result<Pokemon, InsertError> {
+        let mut lock = match self.connection.lock() {
+            Ok(lock) => lock,
+            _ => return Err(InsertError::Unknown),
+        };
+
+        let transaction = match lock.transaction() {
+            Ok(transaction) => transaction,
+            _ => return Err(InsertError::Unknown),
+        };
+
+        match transaction.execute(
+            "insert into pokemons (number, name) values (?, ?)",
+            params![u16::from(number.clone()), String::from(name.clone())],
+        ) {
+            Ok(_) => {}
+            Err(SqliteFailure(_, Some(message))) => {
+                if message == "UNIQUE constraint failed: pokemons.number" {
+                    return Err(InsertError::Conflict);
+                } else {
+                    return Err(InsertError::Unknown);
+                }
+            }
+            _ => return Err(InsertError::Unknown),
+        };
+
+        for _type in Vec::<String>::from(types.clone()) {
+            if let Err(_) = transaction.execute(
+                "insert into types (pokemon_number, name) values (?, ?)",
+                params![u16::from(number.clone()), _type],
+            ) {
+                return Err(InsertError::Unknown);
+            }
+        }
+
+        match transaction.commit() {
+            Ok(_) => Ok(Pokemon::new(number, name, types)),
+            _ => Err(InsertError::Unknown),
+        }
+    }
+
+    fn fetch_all(&self) -> Result<Vec<Pokemon>, FetchAllError> {
+        let lock = match self.connection.lock() {
+            Ok(lock) => lock,
+            _ => return Err(FetchAllError::Unknown),
+        };
+
+        let pokemon_rows = match Self::fetch_pokemon_rows(&lock, None) {
+            Ok(pokemon_rows) => pokemon_rows,
+            _ => return Err(FetchAllError::Unknown),
+        };
+
+        let mut pokemons = vec![];
+
+        for pokemon_row in pokemon_rows {
+            let type_rows = match Self::fetch_type_rows(&lock, pokemon_row.0) {
+                Ok(type_rows) => type_rows,
+                _ => return Err(FetchAllError::Unknown),
+            };
+
+            let pokemon = match (
+                PokemonNumber::try_from(pokemon_row.0),
+                PokemonName::try_from(pokemon_row.1),
+                PokemonTypes::try_from(type_rows),
+            ) {
+                (Ok(number), Ok(name), Ok(types)) => Pokemon::new(number, name, types),
+                _ => return Err(FetchAllError::Unknown),
+            };
+
+            pokemons.push(pokemon);
+        }
+
+        Ok(pokemons)
+    }
+
+    fn fetch_one(&self, number: PokemonNumber) -> Result<Pokemon, FetchOneError> {
+        let lock = match self.connection.lock() {
+            Ok(lock) => lock,
+            _ => return Err(FetchOneError::Unknown),
+        };
+
+        let mut pokemon_rows =
+            match Self::fetch_pokemon_rows(&lock, Some(u16::from(number.clone()))) {
+                Ok(pokemon_rows) => pokemon_rows,
+                _ => return Err(FetchOneError::Unknown),
+            };
+
+        if pokemon_rows.is_empty() {
+            return Err(FetchOneError::NotFound);
+        }
+
+        let pokemon_row = pokemon_rows.remove(0);
+
+        let type_rows = match Self::fetch_type_rows(&lock, pokemon_row.0) {
+            Ok(type_rows) => type_rows,
+            _ => return Err(FetchOneError::Unknown),
+        };
+
+        match (
+            PokemonNumber::try_from(pokemon_row.0),
+            PokemonName::try_from(pokemon_row.1),
+            PokemonTypes::try_from(type_rows),
+        ) {
+            (Ok(number), Ok(name), Ok(types)) => Ok(Pokemon::new(number, name, types)),
+            _ => Err(FetchOneError::Unknown),
+        }
+    }
+
+    fn delete(&self, number: PokemonNumber) -> Result<(), DeleteError> {
+        let lock = match self.connection.lock() {
+            Ok(lock) => lock,
+            _ => return Err(DeleteError::Unknown),
+        };
+
+        match lock.execute(
+            "delete from pokemons where number = ?",
+            params![u16::from(number)],
+        ) {
+            Ok(0) => Err(DeleteError::NotFound),
+            Ok(_) => Ok(()),
+            _ => Err(DeleteError::Unknown),
+        }
     }
 }
